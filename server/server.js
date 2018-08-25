2bf0499b4b1b47b0d717060fe439b0a49c88e18f
William Ma <william.ma@spidertracks.co.nz>
2018-08-25 22:37:18 +1200

    Add Test DB

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require("./config/config");
+
 const _ = require("lodash");
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,7 +12,7 @@ var { validateID, todoFindById } = require('./../playground/mongoose-queries');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
 
 app.use(bodyParser.json());
 
